e82613600673539f1dc1113a65a87281e0ae3c95
Oliver Lemke <olemke@gmail.com>
2018-07-18 14:11:01 +0200

    More docs.

--- xsec_process_species.py
+++ xsec_process_species.py
@@ -12,6 +12,7 @@ logging.basicConfig(level=logging.INFO)
 
 
 def parse_args():
+    """Parse command line arguments."""
     args = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     args.add_argument('species', help='Name of species to process.')
@@ -36,6 +37,7 @@ def main():
     xfi = hx.XsecFileIndex(directory=args.directory, species=args.species,
                            ignore='.*[^0-9._].*')
 
+    # Scatter plot of available cross section data files
     hx.plotting.plot_available_xsecs(xfi, title=args.species)
     plt.gcf().savefig(os.path.join(output_dir, 'xsec_datasets.pdf'))
     plt.gcf().clear()
@@ -48,6 +50,7 @@ def main():
         rms_result = hx.optimize_xsec_multi(xfi)
         hx.save_rms_data(rms_file, rms_result)
 
+    # Plot of best FWHM vs. pressure difference and the fit
     hx.plotting.scatter_and_fit(xfi, rms_result, outliers=False)
     plt.gcf().savefig(os.path.join(output_dir, 'xsec_scatter.pdf'))
     plt.gcf().clear()
