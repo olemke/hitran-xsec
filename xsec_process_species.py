fb626cd6eca779740b094f5f5997dd02817ea6a5
Oliver Lemke <olemke@gmail.com>
2018-07-30 08:43:58 +0200

    Rework the whole control script to offer more options.

--- xsec_process_species.py
+++ xsec_process_species.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 import matplotlib.pyplot as plt
+import typhon
 import typhon.arts.xml as axml
 
 import hitran_xsec as hx
@@ -83,58 +84,143 @@ RFMIP_SPECIES = [
 ]
 
 
+def parse_args():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    # Optional commandline arguments
+    parser.add_argument('-d', '--directory', default='.',
+                        help='Directory with HITRAN cross section data files.')
+    parser.add_argument('-o', '--output', metavar='OUTPUT_DIRECTORY',
+                        default='output',
+                        help='Output directory. A subdirectory named SPECIES '
+                             'will be created inside.')
+    parser.add_argument('-s', '--style', type=str, default=None,
+                        help='Custom matplotlib style name.')
+
+    # RMS command parser
+    subparsers = parser.add_subparsers()
+    rmsparser = subparsers.add_parser('rms',
+                                      help='Calculate rms and perform fitting.')
+    rmsparser.add_argument('-i', '--ignore-rms', action='store_true',
+                           help='Ignore existing RMS file (recalculate).')
+    rmsparser.add_argument('-r', '--rms-plots', action='store_true',
+                           help='Generate cross section and rms plots.')
+    rmsparser.set_defaults(execute=rms_and_fitting)
+
+    # tfit command parser
+    tfitparser = subparsers.add_parser('tfit',
+                                       help='Analyze temperature behaviour.')
+    tfitparser.set_defaults(execute=analyze_temperature)
+
+    # Required commandline argument
+    parser.add_argument('species', metavar='SPECIES', nargs='+',
+                        help='Name of species to process. '
+                             'Pass "rfmip" for all RFMIP species.')
+
+    return parser.parse_args()
+
+
+def prepare_data(directory, output_dir, species):
+    xfi = hx.XsecFileIndex(directory=directory, species=species,
+                           ignore='.*[^0-9._].*')
+    if xfi.files:
+        os.makedirs(output_dir, exist_ok=True)
+    return xfi
+
+
+def analyze_temperature(species, args):
+    output_dir = os.path.join(args.output, species)
+
+    xfi = prepare_data(args.directory, args.output, species)
+    if not xfi.files:
+        logger.warning(f'No input files found for {species}.')
+        return
+
+    bands = xfi.cluster_by_band_and_pressure()
+
+    for band in bands:
+        for pressure in band:
+            if len(pressure) > 3:
+                tpressure = sorted(pressure, key=lambda x: x.temperature)
+                fig, ax = plt.subplots()
+                for xsec1, xsec2 in zip(tpressure[:-1], tpressure[1:]):
+                    ax.plot(xsec2.data - xsec1.data,
+                            label=f'{xsec2.temperature:.0f}K'
+                                  f'-{xsec1.temperature:.0f}K'
+                                  f'={xsec2.temperature-xsec1.temperature:.0f}')
+                ax.legend()
+                plotfile = os.path.join(
+                    output_dir,
+                    f'xsec_temperature_change_'
+                    f'{tpressure[0].wmin:.0f}-{tpressure[0].wmax:.0f}_'
+                    f'{tpressure[0].pressure:.0f}P.pdf')
+                plt.savefig(plotfile)
+                logger.info(f'Wrote {plotfile}')
+
+    pass
+
+
+def rms_and_fitting(species, args):
+    output_dir = os.path.join(args.output, species)
+
+    xfi = prepare_data(args.directory, output_dir, species)
+    if not xfi.files:
+        logger.warning(f'No input files found for {species}.')
+        return
+
+    # Scatter plot of available cross section data files
+    plotfile = os.path.join(output_dir, 'xsec_datasets.pdf')
+    plt.figure()
+    hx.plotting.plot_available_xsecs(xfi, title=species)
+    plt.savefig(plotfile)
+    logger.info(f'Wrote {plotfile}')
+
+    rms_file = os.path.join(output_dir, 'xsec_rms.json')
+    if os.path.exists(rms_file) and not args.ignore_rms:
+        logger.info(f'Reading precalculated RMS values form {rms_file}.')
+        rms_result = hx.xsec.load_rms_data(rms_file)
+    else:
+        rms_result = [x for x in hx.optimize_xsec_multi(xfi) if x]
+        if rms_result:
+            hx.save_rms_data(rms_file, rms_result)
+            logger.info(f'Wrote {rms_file}')
+        else:
+            logger.warning(f'No results for {species}')
+
+    # Plot of best FWHM vs. pressure difference and the fit
+    if rms_result:
+        xml_file = os.path.join(output_dir, 'cfc.xml')
+        axml.save(hx.fit.gen_arts(xfi, rms_result), xml_file)
+        logger.info(f'Wrote {xml_file}')
+
+        plotfile = os.path.join(output_dir, 'xsec_scatter.pdf')
+        plt.figure()
+        hx.plotting.scatter_and_fit(xfi, rms_result, outliers=False)
+        plt.savefig(plotfile)
+        logger.info(f'Wrote {plotfile}')
+
+        if args.rms_plots:
+            for r in rms_result:
+                hx.plotting.generate_rms_and_spectrum_plots(
+                    xfi, title=species, xsec_result=r, outdir=output_dir)
+
+
 def main():
+    typhon.plots.styles.use('typhon')
+
+    global logger
     logger = logging.getLogger(__name__)
     args = parse_args()
 
+    if args.style:
+        plt.style.use(args.style)
+
     if args.species[0] == 'rfmip':
         args.species = RFMIP_SPECIES
+
     for species in args.species:
-        output_dir = os.path.join(args.output, species)
-
-        xfi = hx.XsecFileIndex(directory=args.directory, species=species,
-                               ignore='.*[^0-9._].*')
-        if xfi.files:
-            os.makedirs(output_dir, exist_ok=True)
-        else:
-            logger.warning(f'No input files found for {species}.')
-            continue
-
-        # Scatter plot of available cross section data files
-        plotfile = os.path.join(output_dir, 'xsec_datasets.pdf')
-        hx.plotting.plot_available_xsecs(xfi, title=species)
-        plt.gcf().savefig(plotfile)
-        plt.gcf().clear()
-        logger.info(f'Wrote {plotfile}')
-
-        rms_file = os.path.join(output_dir, 'xsec_rms.json')
-        if os.path.exists(rms_file) and not args.ignore_rms:
-            logger.info(f'Reading precalculated RMS values form {rms_file}.')
-            rms_result = hx.xsec.load_rms_data(rms_file)
-        else:
-            rms_result = [x for x in hx.optimize_xsec_multi(xfi) if x]
-            if rms_result:
-                hx.save_rms_data(rms_file, rms_result)
-                logger.info(f'Wrote {rms_file}')
-            else:
-                logger.warning(f'No results for {species}')
-
-        # Plot of best FWHM vs. pressure difference and the fit
-        if rms_result:
-            xml_file = os.path.join(output_dir, 'cfc.xml')
-            axml.save(hx.fit.gen_arts(xfi, rms_result), xml_file)
-            logger.info(f'Wrote {xml_file}')
-
-            plotfile = os.path.join(output_dir, 'xsec_scatter.pdf')
-            hx.plotting.scatter_and_fit(xfi, rms_result, outliers=False)
-            plt.gcf().savefig(plotfile)
-            plt.gcf().clear()
-            logger.info(f'Wrote {plotfile}')
-
-            if args.rms_plots:
-                for r in rms_result:
-                    hx.plotting.generate_rms_and_spectrum_plots(
-                        xfi, title=species, xsec_result=r, outdir=output_dir)
+        args.execute(species, args)
 
     return 0
 
