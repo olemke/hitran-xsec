6d6638061a36a91f56d3cecc0fba28c1b9ee1046
Oliver Lemke <olemke@gmail.com>
2019-02-12 14:07:53 +0100

    Split arg parsing into smaller functions.

--- xsec_process_species.py
+++ xsec_process_species.py
@@ -1,8 +1,8 @@
 import argparse
 import logging
+import sys
 
 import matplotlib.pyplot as plt
-import sys
 import typhon
 
 from hitran_xsec import (calc_broadening, calc_temperature_correction,
@@ -14,6 +14,64 @@ set_default_logging_format(level=logging.INFO,
                            include_timestamp=True,
                            include_function=True),
 logger = logging.getLogger(__name__)
+
+
+def add_rms_parser_args(subparsers):
+    # RMS command parser
+    subparser = subparsers.add_parser('rms',
+                                      help='Calculate rms and perform fitting.')
+    subparser.add_argument('-i', '--ignore-rms', action='store_true',
+                           help='Ignore existing RMS file (recalculate).')
+    subparser.add_argument('-r', '--rms-plots', action='store_true',
+                           help='Generate cross section and rms plots.')
+    subparser.add_argument('-a', '--averaged', action='store_true',
+                           help='Force use of averaged coefficients.')
+    subparser.set_defaults(command='rms')
+    subparser.set_defaults(execute=calc_broadening)
+
+    subparser.add_argument('species', metavar='SPECIES', nargs='+',
+                           help='Name of species to process. '
+                                'Pass "rfmip" for all RFMIP species.')
+
+
+def add_tfit_parser_args(subparsers):
+    # tfit command parser
+    subparser = subparsers.add_parser('tfit',
+                                      help='Analyze temperature behaviour.')
+    subparser.add_argument('-t', '--tref', type=int, default=-1,
+                           help='Reference temperature.')
+    subparser.set_defaults(command='tfit')
+    subparser.set_defaults(execute=calc_temperature_correction)
+
+    # Required commandline argument
+    subparser.add_argument('species', metavar='SPECIES', nargs='+',
+                           help='Name of species to process. '
+                                'Pass "rfmip" for all RFMIP species.')
+
+
+def add_average_parser_args(subparsers):
+    # average command parser
+    # TODO: Implement averaging command
+    subparser = subparsers.add_parser(
+        'avg', help='Calculate average coefficients from reference species.')
+    subparser.set_defaults(command='average')
+    subparser.set_defaults(execute=calc_average_coeffs)
+
+    # Required commandline argument
+    subparser.add_argument('species', metavar='SPECIES', nargs='+',
+                           help='Name of species to process. '
+                                'Pass "reference" for all default species.')
+
+
+def add_arts_parser_args(subparsers):
+    # arts export command parser
+    subparser = subparsers.add_parser('arts', help='Combine data for ARTS.')
+    subparser.set_defaults(command='arts')
+    subparser.set_defaults(execute=combine_data_for_arts)
+
+    subparser.add_argument('species', metavar='SPECIES', nargs='+',
+                           help='Name of species to process. '
+                                'Pass "rfmip" for all RFMIP species.')
 
 
 def parse_args():
@@ -30,58 +88,14 @@ def parse_args():
     parser.add_argument('-s', '--style', type=str, default=None,
                         help='Custom matplotlib style name.')
 
-    # RMS command parser
     subparsers = parser.add_subparsers()
     subparsers.required = True
     subparsers.metavar = 'COMMAND'
-    subparser = subparsers.add_parser('rms',
-                                      help='Calculate rms and perform fitting.')
-    subparser.add_argument('-i', '--ignore-rms', action='store_true',
-                           help='Ignore existing RMS file (recalculate).')
-    subparser.add_argument('-r', '--rms-plots', action='store_true',
-                           help='Generate cross section and rms plots.')
-    subparser.add_argument('-a', '--averaged', action='store_true',
-                           help='Force use of averaged coefficients.')
-    subparser.set_defaults(command='rms')
-    subparser.set_defaults(execute=calc_broadening)
 
-    subparser.add_argument('species', metavar='SPECIES', nargs='+',
-                           help='Name of species to process. '
-                                'Pass "rfmip" for all RFMIP species.')
-
-    # tfit command parser
-    subparser = subparsers.add_parser('tfit',
-                                      help='Analyze temperature behaviour.')
-    subparser.add_argument('-t', '--tref', type=int, default=-1,
-                           help='Reference temperature.')
-    subparser.set_defaults(command='tfit')
-    subparser.set_defaults(execute=calc_temperature_correction)
-
-    # Required commandline argument
-    subparser.add_argument('species', metavar='SPECIES', nargs='+',
-                           help='Name of species to process. '
-                                'Pass "rfmip" for all RFMIP species.')
-
-    # average command parser
-    # TODO: Implement averaging command
-    subparser = subparsers.add_parser(
-        'avg', help='Calculate average coefficients from reference species.')
-    subparser.set_defaults(command='average')
-    subparser.set_defaults(execute=calc_average_coeffs)
-
-    # Required commandline argument
-    subparser.add_argument('species', metavar='SPECIES', nargs='+',
-                           help='Name of species to process. '
-                                'Pass "reference" for all default species.')
-
-    # arts export command parser
-    subparser = subparsers.add_parser('arts', help='Combine data for ARTS.')
-    subparser.set_defaults(command='arts')
-    subparser.set_defaults(execute=combine_data_for_arts)
-
-    subparser.add_argument('species', metavar='SPECIES', nargs='+',
-                           help='Name of species to process. '
-                                'Pass "rfmip" for all RFMIP species.')
+    add_rms_parser_args(subparsers)
+    add_tfit_parser_args(subparsers)
+    add_average_parser_args(subparsers)
+    add_arts_parser_args(subparsers)
 
     return parser.parse_args()
 
